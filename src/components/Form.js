79e7eb21e0903d817a36ee82490526c73c91308a
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-06 23:11:58 +0100

    states

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,30 @@
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import './FormStyle.css';
 
-import React from 'react'
 
 const Form  = () => {
 
+  const [name, setName] = useState("Ernest Chris");
+  const [email, setEmail] = useState("ernestchris@gmail.com");
+  const [subject, setSubject] = useState("Ernest Chris");
+  const [text, setText] = useState("Enter subject...");
+
+
+  
+ 
 
   return (
     <div className='form'>
       <form>
       <label>Your Name</label>
-        <input type='text'/>
+        <input type='text' value={name} onChange={(e)=> setName(e.target.value)}/>
         <label>Email</label>
-        <input type='email'/>
+        <input type='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
         <label>Subject</label>
-        <input type='text'/>
+        <input type='text' value={subject} onChange={(e)=> setSubject(e.target.value)}/>
         <label>Message</label>
-        <textarea rows='6' placeholder='Type your message here...' />
+        <textarea rows='6' placeholder='Type your message here...' type='email' value={text} onChange={(e)=> setText(e.target.value)} />
         
         <Link to='https://github.com/ernestkris6/'>
         <button className='btn'>Submit</button>
@@ -27,4 +35,4 @@ const Form  = () => {
   )
 }
 
-export default Form;
+export default Form; 
