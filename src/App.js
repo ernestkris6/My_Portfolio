d93e2ec17d3f3eb88dce7b848b62dc70a40f6614
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-06 23:01:34 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.css';
-import Home from './routes/Home';
-import About from './routes/About'
-import Project from './routes/Project'
-import Contact from './routes/Contact'
+import Home from './pages/Home';
+import About from './pages/About'
+import Project from './pages/Project'
+import Contact from './pages/Contact'
 import { Route, Routes } from 'react-router-dom';
 
 
